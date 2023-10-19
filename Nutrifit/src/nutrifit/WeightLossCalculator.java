0cc896c6470b5b934178ed46d8d317d73ff6e9a7
Rij1342 <147336586+Rij1342@users.noreply.github.com>
2023-10-19 12:36:15 -0400

    added weight loss calculator class

---
+++ Nutrifit/src/nutrifit/WeightLossCalculator.java
@@ -0,0 +1,56 @@
+package nutrifit;
+import java.sql.Date;
+
+
+
+public class WeightLossCalculator {
+
+    private User user;
+    ReadWriteController controller;
+
+    public WeightLossCalculator(User user) {
+        ReadWriteController controller = new ReadWriteController();
+        this.user= user;
+
+    }
+
+    public double calculateWeightLoss(Date targetDate) throws Exception {
+        // Calculate the total calorie intake and expenditure until the target date
+
+        Date currentDate = new Date(System.currentTimeMillis());
+        HealthInfo info = controller.retrieveDataBetweenDates(currentDate, targetDate);
+        double caloriesGained = info.getCaloriesConsumed();
+        double caloriesBurned = info.getCaloriesBurned();
+
+
+
+        double netCalories = caloriesGained - caloriesBurned;
+
+
+        double potentialWeightChange = netCalories / 7700;
+
+
+        double projectedWeight = user.getWeight() - potentialWeightChange;
+
+        return projectedWeight;
+    }
+
+    /*
+    public static void main(String[] args) {
+
+
+
+
+        WeightLossCalculator calculator = new WeightLossCalculator();
+
+
+        Date targetDate = new Date(); //
+
+
+        double projectedWeight = calculator.calculateWeightLoss(targetDate);
+
+        System.out.println("Projected weight on " + targetDate + ": " + projectedWeight + " kg");
+    }
+
+     */
+}
