e68f1a2a087b5374db501d3ed682087f8966eb4e
sy17york <99999814+sy17york@users.noreply.github.com>
2023-11-12 23:23:19 -0500

    Update and rename Datebase.java to Database.java
    renamed Datebase -> Database

--- Nutrifit/src/nutrifit/Database.java
+++ Nutrifit/src/nutrifit/Database.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public abstract class Datebase {
+public abstract class Database {
 	/* new connector for MySQL
 	public  Connection connect() {  
  
