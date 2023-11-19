497b17c1fdd61494a0fc88b2efd045b8c5d027f0
Rij1342 <147336586+Rij1342@users.noreply.github.com>
2023-11-19 13:55:25 -0500

    Added factory for chart

---
+++ Nutrifit/src/nutrifit/FoodGroupVisualizer.java
@@ -0,0 +1,19 @@
+package nutrifit;
+/*import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.RingPlot;
+import org.jfree.chart.plot.RingPlot3D;
+import org.jfree.chart.plot.dial.StandardDialPlot;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+*/
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class FoodGroupVisualizer {
+}
