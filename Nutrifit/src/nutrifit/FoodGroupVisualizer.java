7fa70f84ed91bafe9a0a05e0adeebf3a33d30761
Rij1342 <147336586+Rij1342@users.noreply.github.com>
2023-11-19 22:34:58 -0500

    Update FoodGroupVisualizer.java

--- Nutrifit/src/nutrifit/FoodGroupVisualizer.java
+++ Nutrifit/src/nutrifit/FoodGroupVisualizer.java
@@ -1,19 +1,62 @@
-package nutrifit;
-/*import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PiePlot;
-import org.jfree.chart.plot.RingPlot;
-import org.jfree.chart.plot.RingPlot3D;
-import org.jfree.chart.plot.dial.StandardDialPlot;
-import org.jfree.chart.title.LegendTitle;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.general.DefaultPieDataset;
-*/
+
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.time.LocalDate;
 
 public class FoodGroupVisualizer {
+
+    private JFrame frame;
+    private JButton btnVisualizeAverageMeal;
+    private JButton btnVisualizeCFG;
+    private FoodGroupChartFactory chartFactory;
+
+    public FoodGroupVisualizer(FoodGroupChartFactory chartFactory) {
+        this.chartFactory = chartFactory;
+        initialize();
+    }
+
+    private void initialize() {
+        frame = new JFrame("Food Group Visualization");
+        frame.setBounds(100, 100, 400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new FlowLayout());
+
+        btnVisualizeAverageMeal = new JButton("Visualize Average Meal");
+        frame.getContentPane().add(btnVisualizeAverageMeal);
+
+        btnVisualizeCFG = new JButton("Visualize CFG");
+        frame.getContentPane().add(btnVisualizeCFG);
+
+        btnVisualizeAverageMeal.addActionListener(e -> visualizeChart("average meal"));
+        btnVisualizeCFG.addActionListener(e -> visualizeChart("CFG"));
+    }
+
+    private void visualizeChart(String chartType) {
+        JFreeChart chart = chartFactory.generateChart(chartType);
+        displayChart(chart);
+    }
+
+    private void displayChart(JFreeChart chart) {
+        ChartPanel chartPanel = new ChartPanel(chart);
+        JFrame chartFrame = new JFrame("Chart Visualization");
+        chartFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        chartFrame.getContentPane().add(chartPanel, BorderLayout.CENTER);
+        chartFrame.setSize(800, 600);
+        chartFrame.setVisible(true);
+    }
+
+    public void setVisible(boolean visible) {
+        frame.setVisible(visible);
+    }
+
+    public static void main(String[] args) {
+        FoodGroupChartFactory chartFactory = new FoodGroupChartFactory();
+        FoodGroupVisualizer visualizer = new FoodGroupVisualizer(chartFactory);
+        visualizer.setVisible(true);
+    }
 }
